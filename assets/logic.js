7a7b7093f5b8561be9333d04a0728d8a65f13e27
Elm <emmal.muniz@gmail.com>
2018-12-04 20:43:06 -0600

    adding files

---
+++ assets/logic.js
@@ -0,0 +1,10 @@
+
+// hamburger menu for nav bar when mobile size
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  //initialize parallax
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
