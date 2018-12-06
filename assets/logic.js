8c7945c89cb62f9317c00a17388e37573b734c77
Elm <emmal.muniz@gmail.com>
2018-12-06 16:47:35 -0600

    fixed hamburger menu

--- assets/logic.js
+++ assets/logic.js
@@ -3,13 +3,13 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  var instances = M.Sidenav.init(elems);
 });
 
   //initialize parallax
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
+  //document.addEventListener('DOMContentLoaded', function() {
+    //var elems = document.querySelectorAll('.parallax');
+    //var instances = M.Parallax.init(elems, options);
 
-    var instance = M.Parallax.getInstance(elem);})
+   // var instance = M.Parallax.getInstance(elem);})
